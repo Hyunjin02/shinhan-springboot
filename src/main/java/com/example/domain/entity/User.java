4ae02b07cf9b9cc7464f3571e5f7ffd2817b34fc
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-09-27 16:34:16 +0900

    user 권한 추가

---
+++ src/main/java/com/example/domain/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name="role")
+    private String role;
+
+    public User(String username, String password, String role){
+        this.username =username;
+        this.password=password;
+        this.role = role;
+
+    }
+
+    public User() {
+
+    }
+
+}
